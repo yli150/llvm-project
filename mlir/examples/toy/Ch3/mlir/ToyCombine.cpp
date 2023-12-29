5f166fddadb0e2f8dac1eddb4aa8e8d76f4353b5
yli150 <victor.y.li@intel.com>
2023-12-28 21:38:45 -0500

    remove constant with reshape folding from toy3

--- mlir/examples/toy/Ch3/mlir/ToyCombine.cpp
+++ mlir/examples/toy/Ch3/mlir/ToyCombine.cpp
@@ -64,6 +64,5 @@ void TransposeOp::getCanonicalizationPatterns(RewritePatternSet &results,
 /// that they can be picked up by the Canonicalization framework.
 void ReshapeOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
-  results.add<ReshapeReshapeOptPattern, RedundantReshapeOptPattern,
-              FoldConstantReshapeOptPattern>(context);
+  results.add<ReshapeReshapeOptPattern>(context);
 }
